1f682faf4c6bf471ae54936047a8f189fe5d4270
Francesco Lovascio <francesco.lovascio@ens-lyon.fr>
2022-09-16 18:39:12 +0200

    working on fortan example

--- examples/FortranExample.f90
+++ examples/FortranExample.f90
@@ -1,6 +1,17 @@
 
 program example
-  
-  ! This is a comment line; it is ignored by the compiler
-  print *, 'Hello, World!'
+  use iso_c_binding
+  implicit none
+
+  interface
+     function  buildConductivityF(nme) bind(c,name="buildConductivity")
+       import
+       character(kind=c_char), dimension(*) :: nme
+       type(c_ptr) :: buildConductivityF
+     end function buildConductivityF
+  end interface
+  character(len=100,kind=c_char)::file
+  type(c_ptr) :: ret 
+  file = "/Users/fra/Code/opacity-tools/new_cons/Normal_silicates/"
+  ret = buildConductivityF(file) 
 end program example
