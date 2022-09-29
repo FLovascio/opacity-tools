e776fb7025913e1aa3a2d97a512f4d53a567472b
Francesco Lovascio <francesco.lovascio@ens-lyon.fr>
2022-09-29 16:20:02 +0200

    utils

--- src/utils.hxx
+++ src/utils.hxx
@@ -7,11 +7,20 @@
 
 namespace utils{
   template<class T,class... argType>
-  std::vector<T>& brodcast(std::function<T(T,argType...)>,std::vector<T> &x, std::vector<T> &y){
-
+  std::vector<T>& brodcast(std::function<T(T,argType...)> && f_in,const std::vector<T> &x, std::vector<T> &y, argType... args){
+    auto f = std::move(f_in);
+    for(int i=0;i<x.size();++i){
+      y[i]=f(x[i],args...);
+    }
+    return *y;
   }
   template<class T,class... argType>
-  std::vector<T> brodcast(std::function<T(T,argType...)>,std::vector<T> &x){
-
+  std::vector<T> brodcast(std::function<T(T,argType...)> && f_in,std::vector<T> &x, argType... args){
+    auto f = std::move(f_in);
+    std::vector<T> y((T)0,x.size());
+    for(int i=0;i<x.size();++i){
+      y[i]=f(x[i],args...);
+    }
+    return y;
   }
-}
+};
