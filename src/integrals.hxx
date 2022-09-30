b39163c50aec7e05c10a5baad195cea1d7b92cd0
Francesco Lovascio <francesco.lovascio@ens-lyon.fr>
2022-09-30 14:42:13 +0200

    composite naive and composite trapezoid integrals implemented

--- src/integrals.hxx
+++ src/integrals.hxx
@@ -43,6 +43,27 @@ namespace integrals{
     }
   };
   template<class T>
+  class Composite{
+    std::vector<T> *data_x;
+    Composite(std::vector<T> &dx){
+      data_x=*dx;
+    }
+  };
+  template<class T>
+  class CompositeTrapezoid{
+    std::vector<T> *data_x;
+    CompositeTrapezoid(std::vector<T> &dx){
+      data_x=*dx;
+    }
+  }; 
+  template<class T>
+  class CompositeSimpson{
+    std::vector<T> *data_x;
+    CompositeSimpson(std::vector<T> &dx){
+      data_x=*dx;
+    }
+  }; 
+  template<class T>
   void computeWeights(Simpson<T> &v){
     v.weights[0]=(T)1.0;
     for(int i=1;i<v.weights.size();i+=2){
@@ -69,8 +90,42 @@ namespace integrals{
   };
   template<class T>
   void computeWeights(Trapezoid<T> &v){
-
+    v.weights[0]=(T)0.5;
+    for(int i=1;i<v.weights.size();++i){
+      v.weights[i]=(T)1.0;
+    }
+    v.weights[v.weights.size()-1]=(T)0.5;
   };
+  template<class T>
+  T computeIntegral(const std::vector<T> &data, Simpson<T> weights){
+
+  }
+  template<class T>
+  T computeIntegral(const std::vector<T> &data, Simpson38<T> weights){
+
+  }
+  template<class T>
+  T computeIntegral(const std::vector<T> &data, Trapezoid<T> weights){
+
+  }
+  template<class T>
+  T computeIntegral(const std::vector<T> &data, Composite<T> method){
+    T integral=std::accumulate(std::begin(data), std::end(data), decltype(data)::value_type(0));
+    T H= method.data_x[0]-method.data_x[data.size()];
+    return integral/H;
+  }
+  template<class T>
+  T computeIntegral(std::vector<T> data, CompositeTrapezoid<T> method){
+    T integral=(T)0.0;
+    for(int i=1;i<data.size();++i){
+      integral+=0.5*(method.data_x[i]-method.data_x[i-1])*(data[i]+data(i-1));
+    }
+    return integral;
+  }
+  template<class T>
+  T computeIntegral(std::vector<T> data, CompositeSimpson<T> method){
+    
+  }
 };
 
 namespace polynomials{
